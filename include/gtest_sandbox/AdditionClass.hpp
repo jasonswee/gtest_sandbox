b23d49982cb33907c7e8f6ef97f79f63d3fd3052
Jason Swee <jason.swee.sw@gmail.com>
2020-03-20 14:01:10 +0900

    Create code from reference online

---
+++ include/gtest_sandbox/AdditionClass.hpp
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <limits>
+#include <random>
+
+namespace my_name_space {
+
+class MyClass {
+  public:
+  int add_two_ints(int a, int b) { return a + b; }
+};
+} // namespace my_name_space
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "ros/ros.h"
+
+#include "AdditionClass.hpp"
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "my_node");
+  ros::NodeHandle n;
+
+  sleep(2);
+  std::cout << "Hello World, I'm a ROS node, I could do something..." << std::endl;
+
+  return 0;
+}
